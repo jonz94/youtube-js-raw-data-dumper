11f5f34ed5c261f23b16522586e3973fc9e0f396
jonz94 <jody16888@gmail.com>
2024-11-18 05:58:19 +0800

    feat: dump data into `data/` directory

--- example-with-login.sh
+++ example-with-login.sh
@@ -1,5 +1,6 @@
 VIDEO_ID=""
-OUTPUT="${VIDEO_ID}-login.output"
-ERROR="${VIDEO_ID}-login.error"
+OUTPUT_DIR="data"
+OUTPUT="${OUTPUT_DIR}/${VIDEO_ID}-login.output"
+ERROR="${OUTPUT_DIR}/${VIDEO_ID}-login.error"
 
 pnpm run --silent start chat --vid="${VIDEO_ID}" --login 2> "${ERROR}.log" 1> "${OUTPUT}.jsonl"
--- example.sh
+++ example.sh
@@ -1,5 +1,6 @@
 VIDEO_ID=""
-OUTPUT="${VIDEO_ID}.output"
-ERROR="${VIDEO_ID}.error"
+OUTPUT_DIR="data"
+OUTPUT="${OUTPUT_DIR}/${VIDEO_ID}.output"
+ERROR="${OUTPUT_DIR}/${VIDEO_ID}.error"
 
 pnpm run --silent start chat --vid="${VIDEO_ID}" 2> "${ERROR}.log" 1> "${OUTPUT}.jsonl"
