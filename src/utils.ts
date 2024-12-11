271a38f785af79018eb09ca6220fa3d56a82316d
jonz94 <jody16888@gmail.com>
2024-12-11 20:09:01 +0800

    🚧 WIP: work in progress...

--- src/utils.ts
+++ src/utils.ts
@@ -100,3 +100,13 @@ export async function getAccountName(youtube: Innertube) {
 
   throw new Error(`parse ${content.type} is not implemented`)
 }
+
+export function convertARGB2rgbString(argb: number) {
+  const argbString = argb.toString(16).padStart(8, '0')
+
+  const [a, r, g, b] = (argbString.substring(0, 8).match(/.{1,2}/g) as [string, string, string, string]).map((hex) =>
+    parseInt(hex, 16),
+  )
+
+  return `rgb(${r},${g},${b},${(a ?? 0) / 255})`
+}
